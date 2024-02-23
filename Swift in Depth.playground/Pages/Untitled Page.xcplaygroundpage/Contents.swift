0a9a419204723d07832a0ab905162f013deba30e
Martynenko Uriy <uriy.martynenko@gmail.com>
2024-02-23 16:54:26 +0300

    Формирование модели для приложения Workout

--- Swift in Depth.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ Swift in Depth.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -1,3 +1,26 @@
 import Cocoa
 import Foundation
 
+
+struct Run {
+    let id: String
+    let startTime: Date
+    let endTime: Date
+    let distance: Float
+    let onRunningTrack: Bool
+}
+
+struct Cycle {
+    enum CycleType {
+        case regular
+        case mountainBike
+        case racetrack
+    }
+    
+    let id: String
+    let startTime: Date
+    let endTime: Date
+    let distance: Float
+    let incline: Int
+    let type: CycleType
+}
