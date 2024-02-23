f955e0cd6a66b4db2fc0fb1037cd619bb2160b29
Martynenko Uriy <uriy.martynenko@gmail.com>
2024-02-23 12:59:15 +0300

    Перечисления для полиморфизма

--- Swift in Depth.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ Swift in Depth.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -2,42 +2,14 @@ import Cocoa
 import Foundation
 
 
-struct Message {
-    let userID: String
-    let contents: String?
-    let date: Date
-    let hasJoined: Bool
-    let hasLeft: Bool
-    let isBeingDrafted: Bool
-    let isSendingBalloons: Bool
+let arr: [Any] = [Date(), "Why was six afraid of seven?", "Because...", 789]
+
+for element: Any in arr {
+    switch element {
+        case let stringValue as String: "received a string: \(stringValue)"
+        case let intValue as Int: "received an Int: \(intValue)"
+        case let dateValue as Date: "received a date: \(dateValue)"
+        default: print("I am not interested in this value")
+    }
 }
-
-
-let joinMessage = Message(
-    userID: "1",
-    contents: nil,
-    date: Date(),
-    hasJoined: true, // Устанавливает значения логического типа данных
-    hasLeft: false,
-    isBeingDrafted: false,
-    isSendingBalloons: false
-)
-let textMessage = Message(
-    userID: "2",
-    contents: "Hey everyone!",  //передаем сообщение
-    date: Date(),
-    hasJoined: false,
-    hasLeft: false,
-    isBeingDrafted: false,
-    isSendingBalloons: false
-)
-let brokenMessage = Message(
-    userID: "1",
-    contents: "Hi there",   //Текст для показа
-    date: Date(),
-    hasJoined: true,
-    hasLeft: true,
-    isBeingDrafted: false,
-    isSendingBalloons: false
-)
 
