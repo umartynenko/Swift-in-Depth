f06b36ab30deb4e6c489c72f5008232ccc5bbdaa
Martynenko Uriy <uriy.martynenko@gmail.com>
2024-02-23 23:43:50 +0300

    Опасность необработанных значений

--- Swift in Depth.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ Swift in Depth.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -2,9 +2,26 @@ import Cocoa
 import Foundation
 
 enum Currency: String {
-    case euro
+    case euro = "eur"
     case usd
     case gbp
 }
 
 print(Currency.euro.rawValue)
+
+let  currency = Currency.euro
+
+print(currency.rawValue)
+
+let parameters: [String: String]
+
+
+switch currency {
+    case .euro: parameters = ["filter": "euro"]
+    case .usd: parameters = ["filter": "usd"]
+    case .gbp: parameters = ["filter": "gbp"]
+}
+
+
+print(parameters)
+
