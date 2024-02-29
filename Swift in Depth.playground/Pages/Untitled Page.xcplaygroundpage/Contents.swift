6c4c10107a576f39e2a08bf1aa33ed90bd01999f
Martynenko Uriy <uriy.martynenko@gmail.com>
2024-03-01 01:44:16 +0300

    2.5.2

--- Swift in Depth.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ Swift in Depth.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -1,27 +1,34 @@
 import Cocoa
 import Foundation
 
-enum Currency: String {
-    case euro = "eur"
-    case usd
-    case gbp
-}
 
-print(Currency.euro.rawValue)
-
-let  currency = Currency.euro
-
-print(currency.rawValue)
-
-let parameters: [String: String]
-
-
-switch currency {
-    case .euro: parameters = ["filter": "euro"]
-    case .usd: parameters = ["filter": "usd"]
-    case .gbp: parameters = ["filter": "gbp"]
+enum ImageType: String {
+    case jpg
+    case bmp
+    case gif
+    
+    init?(rawValue: String) {
+        switch rawValue.lowercased() {
+            case "jpg", "jpeg": self = .jpg
+            case "bmp", "bitmap": self = .bmp
+            case "gif", "gifv": self = .gif
+            default: return nil
+        }
+    }
 }
 
 
-print(parameters)
+func iconName(for fileExtensions: String) -> String {
+    guard let imageType = ImageType(rawValue: fileExtensions) else {return "assetIconUnknown"}
+    
+    switch fileExtensions {
+        case "jpg": return "assetIconJpeg"
+        case "bmp": return "assetIconBitmap"
+        case "gif": return "assetIconGif"
+        default: return "assetIconUnknown"
+    }
+}
 
+
+iconName(for: "jpg")
+iconName(for: "bmp")
